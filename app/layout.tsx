088dd2777bfd42110d44a62565330f53a93733af
Raziel1231 <50638901+Raziel1231@users.noreply.github.com>
2023-05-18 22:22:56 -0600

    feat: Implement the root layout

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "@styles/globals.css";
+
+export const metadata = {
+    title: "AiPrompts",
+    description: "Share AI prompts"
+}
+
+function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+        <html lang="en">
+            <body>
+                <div className="main">
+                    <div className="gradient"></div>
+                </div>
+                
+                <main className="app">
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout;
